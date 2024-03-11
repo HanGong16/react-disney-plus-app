f68828ae037a073420151a1310cae7de84912fc3
gong16 <hgim96715@gmail.com>
2024-03-11 21:41:45 +0900

    feat: Add requests
    -자주 쓰는 requests정리

---
+++ src/api/requests.js
@@ -0,0 +1,11 @@
+const requests = {
+  fetchNowPlaying: '/movie/now_playing',
+  fetchTopRated: '/movie/top_rated',
+  fetchTrending: '/trending/all/week',
+  fetchActionMovies: '/discover/movie?width_genres=28',
+  fetchComedyMovies: '/discover/movie?width_genres=35',
+  fetchHorrorMovies: '/discover/movie?width_genres=27',
+  fetchRomanceMovies: '/discover/movie?width_genres=10749',
+  fetchDocumentaries: '/discover/movie?width_genres=99',
+};
+export default requests;
