3b47f06eb1d2763f59f86979e68b75d93b3af1ff
gong16 <hgim96715@gmail.com>
2024-03-11 21:50:52 +0900

    style: App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import styled from 'styled-components';
 import Nav from './components/Nav';
 
 function App() {
   return (
-    <div>
+    <Container>
       <Nav />
-    </div>
+    </Container>
   );
 }
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 24px);
+
+  &:after {
+    content: '';
+    position: absolute;
+    background-image: url('../public/images/home-background.png');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: center;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
 
 export default App;
