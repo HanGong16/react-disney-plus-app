e82b7d27bc3093a62be2f579960646b252d4b862
gong16 <hgim96715@gmail.com>
2024-03-20 20:38:05 +0900

    docs: firebase
    REACT->REACT_APP

--- src/firebase.js
+++ src/firebase.js
@@ -3,10 +3,10 @@ import 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_AUTH_DOMAIN,
-  projectId: process.env.REACT_PROJECT_ID,
-  storageBucket: process.env.REACT_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_MESSAGE_SENDER,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
   appId: process.env.REACT_APP_ID,
 };
 
