65e00657f911b9f55e31bf03d4458916739efd80
gong16 <hgim96715@gmail.com>
2024-03-20 22:22:45 +0900

    docs: remove navList comment

--- src/data/navLists.js
+++ src/data/navLists.js
@@ -1,9 +1,5 @@
 import { IoMdHome } from 'react-icons/io';
 import { IoMdSearch } from 'react-icons/io';
-import { BiLogIn } from 'react-icons/bi';
-// import { FaStar } from 'react-icons/fa';
-// import { RiMovie2Fill } from 'react-icons/ri';
-// import { PiTelevisionFill } from 'react-icons/pi';
 
 export const navLists = [
   {
@@ -16,24 +12,4 @@ export const navLists = [
     title: '검색',
     url: 'search',
   },
-  {
-    icon: <BiLogIn />,
-    title: 'login',
-    url: 'login',
-  },
-  // {
-  //   icon: <FaStar />,
-  //   title: '오리지널',
-  //   url: '/originals',
-  // },
-  // {
-  //   icon: <RiMovie2Fill />,
-  //   title: '영화',
-  //   url: '/movies',
-  // },
-  // {
-  //   icon: <PiTelevisionFill />,
-  //   title: '시리즈',
-  //   url: '/series',
-  // },
 ];
